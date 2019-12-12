9249f11d974cbe698c2d9ec9e0dcac0f975165a1
jero <aneurysmjs@outlook.com>
2019-12-12 22:05:11 +0300

    improvement(footer): apply correct typing

--- src/components/core/Footer/Footer.tsx
+++ src/components/core/Footer/Footer.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-const Footer = () => (
+const Footer: FunctionComponent = () => (
   <footer className="text-center">
     <div className="container">
       <div className="row">
-        <div className="col-lg-12">
-          { new Date().getFullYear() }
-        </div>
+        <div className="col-lg-12">{new Date().getFullYear()}</div>
       </div>
     </div>
   </footer>
