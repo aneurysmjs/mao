15ae7cdc6c9cfe0296b324b1de957b27d8b29c62
jero <aneurysmjs@outlook.com>
2019-12-14 19:04:55 +0300

    improvement(footer): import YEAR from constants

--- src/components/core/Footer/Footer.tsx
+++ src/components/core/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react';
 
+import { YEAR } from '~/constants/';
+
 const Footer: FunctionComponent = () => (
   <footer className="text-center">
     <div className="container">
       <div className="row">
-        <div className="col-lg-12">{new Date().getFullYear()}</div>
+        <div className="col-lg-12">{YEAR}</div>
       </div>
     </div>
   </footer>
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,2 +1,3 @@
 export const RESOLUTION = 40;
 export const SIZE = 400;
+export const YEAR = new Date().getFullYear();
