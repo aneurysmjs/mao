4a31ff1fab0d9c74531bf0791af148a4bb164446
jero <aneurysmjs@outlook.com>
2019-12-14 16:00:28 +0300

    improvement(genrandom): fix type for param

--- src/utils/genRandom.tsx
+++ src/utils/genRandom.tsx
@@ -1,4 +1,4 @@
 // make a random number between 1 and selected bound
-export default function genRandom(bound: number = 2): number {
+export default function genRandom(bound = 2): number {
   return Math.floor(Math.random() * bound);
 };
