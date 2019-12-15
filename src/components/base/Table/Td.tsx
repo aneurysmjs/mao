320df380b5323e4c0f799db197fb6b2b9413fb8d
jero <aneurysmjs@outlook.com>
2019-12-16 00:16:17 +0300

    improvement(td): add 'isHovered' prop

--- src/components/base/Table/Td.tsx
+++ src/components/base/Table/Td.tsx
@@ -1,16 +1,25 @@
 import React, { FunctionComponent, ReactNode } from 'react';
+import classNames from 'classnames';
 
 interface PropsType {
   children?: ReactNode;
+  col?: number;
+  row?: number;
+  isHover?: boolean;
   style?: {
     [key: string]: string | number;
   };
-  col?: number;
-  row?: number;
 }
 
-const Td: FunctionComponent<PropsType> = ({ children, style, col, row }: PropsType) => (
-  <div className="table__td" data-col={col} data-row={row} style={style || {}}>
+const Td: FunctionComponent<PropsType> = ({ children, style, col, row, isHover }: PropsType) => (
+  <div
+    className={classNames('table__td', {
+      'table__td--hovered': isHover,
+    })}
+    data-col={col}
+    data-row={row}
+    style={style || {}}
+  >
     {children}
   </div>
 );
